e0902b4e292939ea3544ac51bb6bcab09e7758b0
NaikyDing <74ding@gmail.com>
2021-03-18 10:32:41 +0800

    ✨feat: Two Sum

--- 01.TwoSum/index.js
+++ 01.TwoSum/index.js
@@ -1,27 +1,29 @@
-var twoSum = function(nums, target) {    
+/**
+ * nums 陣列，其中兩數相加為 target
+ * @date 2021-03-18
+ * @param {number[]} nums
+ * @param {number} target
+ * @returns {number[]} 相加為 target 的兩數索引
+ */
+const twoSum = function (nums, target) {
   let ary = []
   let finalAry = []
-  
-  nums.forEach((item, index)=>{
+
+  nums.forEach((val, index) => {
     let copyAry = [...nums]
     copyAry.splice(index, 1)
-      copyAry.forEach((otherVal)=>{ 
-        console.log(otherVal + item == target)
-          if(otherVal + item == target) {
-            ary.push(otherVal)
-            return false
-          }
-      })
-      
+    copyAry.forEach((otherVal) => {
+      if (otherVal + val == target) {
+        if (ary.length == 2) return false
+        ary.push(otherVal, val)
+        return false
+      }
+    })
   })
-  
-  if(ary.length > 1) {
-      ary.forEach(val=>{
-          finalAry.push(nums.indexOf(val))
-      })
+  ary.forEach((val) => {
+    finalAry.push(nums.indexOf(val))
+  })
+  return finalAry.sort()
+}
 
-      return finalAry.sort()
-  }  
-};
-
-console.log(twoSum([2,7,11,15], 9))
+console.log(twoSum([2, 7, 11, 15], 9))
